245534bf67391351fc1e4dda7f4db5a8114382f8
julemalysheva <julemalysheva@gmail.com>
2024-01-20 16:05:55 +0500

    task3 seminar4 thymeleaf, Lombok

---
+++ Lesson4/task_3/src/main/java/com/example/task_3/models/Product.java
@@ -0,0 +1,12 @@
+package com.example.task_3.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String name;
+
+    private double price;
+
+}
---
+++ Lesson4/task_3/src/main/java/com/example/task_3/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.example.task_3.services;
+
+import org.springframework.stereotype.Service;
+import com.example.task_3.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public List<Product> getAllProducts(){
+        return products;
+    }
+
+}
