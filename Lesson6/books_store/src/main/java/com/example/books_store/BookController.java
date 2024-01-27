b82074ec5a127327ee56534d6957f6f336871173
julemalysheva <julemalysheva@gmail.com>
2024-01-27 17:45:09 +0500

    example Lec6 added springdoc-openapi-starter-webmvc-ui

--- Lesson6/books_store/src/main/java/com/example/books_store/BookController.java
+++ Lesson6/books_store/src/main/java/com/example/books_store/BookController.java
@@ -1,8 +1,6 @@
 package com.example.books_store;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
