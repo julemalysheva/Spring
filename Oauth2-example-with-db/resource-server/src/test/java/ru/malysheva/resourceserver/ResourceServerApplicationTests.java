ca56a9f7f7141bee1277fb200ffde3eea75f574c
julemalysheva <julemalysheva@gmail.com>
2024-02-04 20:21:48 +0500

    Spring Boot Oauth2-example-with-db

---
+++ Oauth2-example-with-db/resource-server/src/test/java/ru/malysheva/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.malysheva.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
